de6dea7cbc9508a71c5986b3a990d6a20c3d0691
farrux2213 <asqaraliyevfarrux2213@gmail.com>
2024-02-17 09:48:08 +0500

    problem 3

---
+++ index.js
@@ -0,0 +1,14 @@
+function tubSonTopish(son) {
+  if (son <= 1) {
+    return false;
+  }
+  for (let i = 2; i <= Math.sqrt(son); i++) {
+    if (son % i === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(tubSonTopish(7)); // true
+console.log(tubSonTopish(14)); // false
