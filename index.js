4f57af2348abe89f2d278eabbf2310885e255872
farrux2213 <asqaraliyevfarrux2213@gmail.com>
2024-02-21 21:27:01 +0500

    promlem3

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
-function tubSonTopish(son) {
-  if (son <= 1) {
-    return false;
-  }
-  for (let i = 2; i <= Math.sqrt(son); i++) {
-    if (son % i === 0) {
-      return false;
-    }
-  }
-  return true;
-}
+// function tubSonTopish(son) {
+//   if (son <= 1) {
+//     return false;
+//   }
+//   for (let i = 2; i <= Math.sqrt(son); i++) {
+//     if (son % i === 0) {
+//       return false;
+//     }
+//   }
+//   return true;
+// }
 
-console.log(tubSonTopish(7)); // true
-console.log(tubSonTopish(14)); // false
+// console.log(tubSonTopish(7)); // true
+// console.log(tubSonTopish(14)); // false
+
+//problem4
+function ortachaBaho() {
+  var talaba = {
+    ismi: "Farruxbek",
+    kursi: 1,
+    falsafa_fani: 4,
+    canculs_fani: 3,
+    IT_fani: 5,
+  };
+
+  var sum_baholar = talaba.falsafa_fani + talaba.canculs_fani + talaba.IT_fani;
+  var sum_fanlar_soni = 3;
+  ortachaBaho();
+  console.log(sum_baholar / sum_fanlar_soni);
+}
